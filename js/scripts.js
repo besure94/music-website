5ec734a25c4aed242db462512001761a4477d7d7
Brian Scherner <brianscherner@gmail.com>
2023-05-01 12:06:00 -0700

    Add window.onload function. Declare function for random number generator. Declare mouseover event to update body background color using random number generator function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
-function getRandomArbitrary(min, max) {
-	return Math.random() * (max - min) + min;
-}
+window.onload = function() {
+	function getRandom(max) {
+		return Math.floor(Math.random() * max);
+	};
+
+	let body = document.querySelector("body");
+	body.onmouseover = function() {
+		let colorA = getRandom(255);
+		let colorB = getRandom(255);
+		let colorC = getRandom(255);
+		let rgbString = `rgb(${colorA}, ${colorB}, ${colorC})`
+		body.style.backgroundColor = rgbString;
+		console.log(rgbString);
+	};
+};
